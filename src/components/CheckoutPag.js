7d13811e72e0015d3a0dd765292e134e5660a588
Alejandro-va <prof.jahz@gmail.com>
2021-12-30 16:39:48 -0300

    primera parte

---
+++ src/components/CheckoutPag.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+//import CheckoutCard from "./CheckoutCard";
+import Total from "./Total";
+import products from "../product-data";
+import Product from "./Product";
+import CheckoutCard from "./CheckoutCard";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: "2rem",
+  },
+}));
+
+const CheckoutPag = () => {
+  const classes = useStyles();
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+        {products.map((elemt) => (
+          <Grid item xs={12} sm={8} md={6} lg={4}>
+            <CheckoutCard key={elemt.id} productsCommerce={elemt} />
+            {console.log(elemt.id)}
+          </Grid>
+        ))}
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography align="center" gutterBottom variant="h4">
+            Shopping Cart
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={8} md={9} container spacing={2}>
+          <FormRow />
+          {/* grid anidado  con la funcion arriba, son prductos*/}
+        </Grid>
+        <Grid item xs={12} sm={4} md={3}>
+          <Typography align="center" gutterBottom variant="h4">
+            <Total />
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default CheckoutPag;
+/*       <Grid container spacing={2}>
+        {products.map((el) => (
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Product key={el.id} productsCommerce={el} />
+          </Grid> */
---
+++ src/product-data.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 1,
+    name: "Roller  shoes",
+    productType: "roller skates shoes",
+    price: 50,
+    rating: 4,
+    image: "https://m.media-amazon.com/images/I/61xLiKVGtEL._AC_UY395_.jpg",
+    description: "roller skates shoes",
+  },
+  {
+    id: 2,
+    name: "Laptop",
+    productType: "Tecnology",
+    price: 500,
+    rating: 4,
+    image:
+      "https://ideakreativa.net/wp-content/uploads/2020/04/laptop-para-disen%CC%83adores.jpg",
+    description: "Tecnologia",
+  },
+  {
+    id: 3,
+    name: "Clothes",
+    productType: "Pants",
+    price: 1500,
+    rating: 4,
+    image:
+      "https://laopinion.com/wp-content/uploads/sites/3/2020/02/pantalones-mujer.jpg?quality=80&strip=all&w=1200",
+    description: "Pantalones para ellas",
+  },
+  {
+    id: 4,
+    name: "Cat",
+    productType: "Pets",
+    price: 2000,
+    rating: 5,
+    image:
+      "https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/social_share_large/public/purina-10-datos-curiosos-sobre-los-gatos.png?itok=88pMyzkl",
+    description: "Mascotas para la familia",
+  },
+  {
+    id: 5,
+    name: "Shoes",
+    productType: "Sport shoes",
+    price: 6000,
+    rating: 5,
+    image: "https://aws.glamour.es/prod/designs/v1/assets/643x502/711719.jpg",
+    description: "Sport shoes",
+  },
+];
+
+export default products;

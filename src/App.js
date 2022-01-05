babf463dfafc2686442edc06d5e5635c82ecf05f
Alejandro-va <prof.jahz@gmail.com>
2022-01-05 16:36:20 -0300

    apunto de implemetar auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import CheckoutPag from "./components/CheckoutPag";
 import { Routes, BrowserRouter, Route } from "react-router-dom";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/checkout-page" element={<CheckoutPag />} />
           <Route path="/" element={<Products />} />
         </Routes>
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMOwy2lRbddSvyv9NXUxxDctxyUJg47Oo",
+  authDomain: "carro-de-compras-react.firebaseapp.com",
+  projectId: "carro-de-compras-react",
+  storageBucket: "carro-de-compras-react.appspot.com",
+  messagingSenderId: "920739482613",
+  appId: "1:920739482613:web:3c3a0681878ce5156deebc",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+export { auth };

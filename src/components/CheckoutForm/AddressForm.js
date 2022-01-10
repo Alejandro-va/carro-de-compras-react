535c48aa1edf9a20bf26c8fae7c62e90c588ec16
Alejandro-va <prof.jahz@gmail.com>
2022-01-10 17:55:14 -0300

    useform

--- src/components/CheckoutForm/AddressForm.js
+++ src/components/CheckoutForm/AddressForm.js
@@ -1,22 +1,50 @@
-import { Grid, Typography } from "@material-ui/core";
-import { useForm, FormProvider } from "react-hook-form";
+import { Grid, Typography, Button, TextField } from "@material-ui/core";
+import { useForm, Controller } from "react-hook-form";
 import AddressInput from "./AddressInput";
+import { Link } from "react-router-dom";
 
 const AddressForm = () => {
-  const methods = useForm();
-
+  const { handleSubmit, control } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <>
       <Typography variant="h6" gutterBottom>
         Shipping address
       </Typography>
-      <FormProvider {...methods}>
-        <form>
-          <Grid container spacing={3}>
-            <AddressInput required name="firstName" label="First Name" />
-          </Grid>
-        </form>
-      </FormProvider>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={3}>
+          <Controller
+            control={control}
+            render={({ field }) => (
+              <TextField fullWidth defaultValue="" name="ddd" label="ffff" />
+            )}
+          />
+          {/*        <AddressInput required name="firstName" label="First Name" />
+           <AddressInput required name="lastName" label="Last Name" />
+          <AddressInput required name="address1" label="Address" />
+          <AddressInput required name="email" label="Email Address" />
+          <AddressInput required name="city" label="City" />
+          <AddressInput required name="postCode" label="Post Code" /> */}
+        </Grid>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "1rem",
+          }}
+        >
+          {/* es lo mismo meter el boton dentro de un link que darle el componente link */}
+          <Button component={Link} to="/checkout-page">
+            Back to the Checkout Page
+          </Button>
+          <Button type="submit" variant="contained" color="primary">
+            Next
+          </Button>
+        </div>
+      </form>
     </>
   );
 };
--- src/components/CheckoutForm/AddressInput.js
+++ src/components/CheckoutForm/AddressInput.js
@@ -1,4 +1,4 @@
-import { useFormContext, Controller } from "react-hook-form";
+/* import { useFormContext, Controller } from "react-hook-form";
 import { Grid, TextField } from "@material-ui/core";
 
 const AddressInput = ({ name, label, required }) => {
@@ -7,13 +7,15 @@ const AddressInput = ({ name, label, required }) => {
     <Grid item xs={12} sm={6}>
       <Controller
         control={control}
-        render={({ field }) => (
+        render={({ field: { onChange, value } }) => (
           <TextField
             fullWidth
             defaultValue=""
             name={name}
             label={label}
             required={required}
+            value={value}
+            onChange={onChange}
           />
         )}
       />
@@ -21,14 +23,51 @@ const AddressInput = ({ name, label, required }) => {
   );
 };
 
-export default AddressInput;
+export default AddressInput; */
 
-/*       <Controller
-        as={TextField}
-        control={control}
-        fullWidth
-        defaultValue=""
-        name={name}
-        label={label}
-        required={required}
-      />; */
+/* 
+import { Grid, Typography, Button } from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+import AddressInput from "./AddressInput";
+import { Link } from "react-router-dom";
+
+const AddressForm = () => {
+  const methods = useForm();
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping address
+      </Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit((data) => console.log(data))}>
+          <Grid container spacing={3}>
+            <AddressInput required name="firstName" label="First Name" />
+            <AddressInput required name="lastName" label="Last Name" />
+            <AddressInput required name="address1" label="Address" />
+            <AddressInput required name="email" label="Email Address" />
+            <AddressInput required name="city" label="City" />
+            <AddressInput required name="postCode" label="Post Code" />
+          </Grid>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: "1rem",
+            }}
+          >
+       
+            <Button component={Link} to="/checkout-page">
+              Back to the Checkout Page
+            </Button>
+            <Button type="submit" variant="contained" color="primary">
+              Next
+            </Button>
+          </div>
+        </form>
+      </FormProvider>
+    </>
+  );
+};
+
+export default AddressForm; */
